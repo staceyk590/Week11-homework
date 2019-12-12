3b4eb0adddd00435b03e8ac3e8ac1b94728617a1
Stacey Koumaras <stacey_k@tpg.com.au>
2019-12-13 09:37:39 +1100

    homework

---
+++ app.js
@@ -0,0 +1,10 @@
+var Employee = require('./lib/Employee');
+
+
+//var emp1 = new Employee('Stacey', 1, "Manager");
+//var emp2 = new Employee('stacey2', 2);
+
+//console.log(emp1.getName());
+//console.log(emp2.getName());
+//console.log(emp1.getRole());
+//console.log(emp2.getRole());
